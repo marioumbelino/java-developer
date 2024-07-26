4d3ce706e454cbd916a3a53202c4bb0f1685255d
marioumbelino <marioherbert2016@gmail.com>
2024-07-26 20:29:01 -0300

    Códigos Utilizados em aula - estrutura de repetição e tratamento de exceção

---
+++ estrutura-controle/estrutura-repeticao/faca-enquanto/DoWhileExemple.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+
+public class DoWhileExemple {
+    public static void main(String[] args) {
+        System.out.println("Discando...");
+
+        do {
+            System.out.println("Telefone tocando");
+        } while (tocando());
+
+        System.out.println("Atendeu");
+    }
+
+    private static boolean tocando() {
+        boolean atendeu = new Random().nextInt(3) == 1;
+        System.out.println("Atendeu? " + atendeu);
+        return !atendeu;
+    }
+}
